0bb90170505454d4485eeae610e9c1e4739cd5dd
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-03 13:06:47 +0530

    added ec2.tf file

---
+++ EC2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_instance" "terraform1" {
+  ami           = “ami-04b4f1a9cf54c11d0”
+  instance_type = "t2.micro"               
+
+  tags = {
+    Name = "GitHub Actions EC2 Instance"
+  }
+}
