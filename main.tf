e444e5b354634b8cf2cb6759dc3c6def229876bb
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-09 14:14:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,57 +1,31 @@
 provider "aws" {
-  region  = "us-east-1"
-}
-
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "MyVPC"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-  tags = {
-    Name = "MySubnet"
-  }
+  region = "us-east-1"
 }
 
 resource "aws_security_group" "my_security_group" {
   name        = "my_security_group"
-  description = "Allow inbound HTTP and SSH"
-  vpc_id      = aws_vpc.my_vpc.id
-
+  description = "Allow all inbound traffic"
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "my_instance1" {
-  ami           = "ami-0c55b159cbfafe1f0"  
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your AMI ID
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.my_subnet.id
-  security_group = aws_security_group.my_security_group.id
+
+  # Correct use of security_groups (plural)
+  security_groups = [aws_security_group.my_security_group.name]
+
   tags = {
     Name = "MyInstance"
   }
