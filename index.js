0ad9555ee584b6e17ed1bb9e5bd789c46884be6a
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-12 07:56:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const core = require('@actions/core');
 
 try {
-  // Retrieve inputs
+  
   const input1 = core.getInput('input1');
   const input2 = core.getInput('input2');
   
-  // Print inputs to the console
   console.log(`Input 1: ${input1}`);
   console.log(`Input 2: ${input2}`);
   
-  // Custom action logic: Example of additional computation or logic
   const result = `Processed result: ${input1} + ${input2}`;
-  console.log(result);
-
   
-  core.setOutput("combinedResult", result);
-
 } catch (error) {
-  // Log any errors and mark the action as failed
+  
   core.setFailed(error.message);
 }
